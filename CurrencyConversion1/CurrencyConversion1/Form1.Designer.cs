8ea6f4a3d22f22136d420a4b6a05e1b1fd3c938a
JakubPawlowski99 <56391766+JakubPawlowski99@users.noreply.github.com>
2024-03-20 15:18:07 +0100

    Add files via upload

---
+++ CurrencyConversion1/CurrencyConversion1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace CurrencyConverterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSellAmount = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBoxSellCurrency = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxBuyCurrency = new System.Windows.Forms.ComboBox();
+            this.txtBuyAmount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "amount";
+            // 
+            // txtSellAmount
+            // 
+            this.txtSellAmount.Location = new System.Drawing.Point(164, 42);
+            this.txtSellAmount.Name = "txtSellAmount";
+            this.txtSellAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtSellAmount.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(471, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "exchange";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnExchange_Click);
+            // 
+            // comboBoxSellCurrency
+            // 
+            this.comboBoxSellCurrency.FormattingEnabled = true;
+            this.comboBoxSellCurrency.Location = new System.Drawing.Point(164, 19);
+            this.comboBoxSellCurrency.Name = "comboBoxSellCurrency";
+            this.comboBoxSellCurrency.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSellCurrency.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "currency sell";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(325, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "currency buy";
+            // 
+            // comboBoxBuyCurrency
+            // 
+            this.comboBoxBuyCurrency.FormattingEnabled = true;
+            this.comboBoxBuyCurrency.Location = new System.Drawing.Point(410, 19);
+            this.comboBoxBuyCurrency.Name = "comboBoxBuyCurrency";
+            this.comboBoxBuyCurrency.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxBuyCurrency.TabIndex = 5;
+            // 
+            // txtBuyAmount
+            // 
+            this.txtBuyAmount.Location = new System.Drawing.Point(410, 49);
+            this.txtBuyAmount.Name = "txtBuyAmount";
+            this.txtBuyAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtBuyAmount.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(325, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "amount";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBuyAmount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxBuyCurrency);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxSellCurrency);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSellAmount);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSellAmount;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBoxSellCurrency;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxBuyCurrency;
+        private System.Windows.Forms.TextBox txtBuyAmount;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CurrencyConversion1/CurrencyConversion1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CurrencyConverterApp
+{
+    public partial class Form1 : Form
+    {
+        // Hardcoded exchange rates (example rates)
+        private readonly double usdToEuroRate = 0.85;
+        private readonly double euroToUsdRate = 1.18;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Populate ComboBoxes with currency options
+            comboBoxSellCurrency.Items.AddRange(new string[] { "USD", "Euro" });
+            comboBoxBuyCurrency.Items.AddRange(new string[] { "USD", "Euro" });
+        }
+
+        private void btnExchange_Click(object sender, EventArgs e)
+        {
+            // Parse the amount to sell
+            if (!double.TryParse(txtSellAmount.Text, out double sellAmount))
+            {
+                MessageBox.Show("Please enter a valid sell amount.");
+                return;
+            }
+
+            // Determine the currencies
+            string sellCurrency = comboBoxSellCurrency.SelectedItem.ToString();
+            string buyCurrency = comboBoxBuyCurrency.SelectedItem.ToString();
+
+            // Perform the conversion
+            double resultAmount;
+            if (sellCurrency == "USD" && buyCurrency == "Euro")
+            {
+                resultAmount = sellAmount * usdToEuroRate;
+            }
+            else if (sellCurrency == "Euro" && buyCurrency == "USD")
+            {
+                resultAmount = sellAmount * euroToUsdRate;
+            }
+            else
+            {
+                MessageBox.Show("Unsupported currency conversion.");
+                return;
+            }
+
+            // Display the result
+            txtBuyAmount.Text = resultAmount.ToString();
+        }
+    }
+}
